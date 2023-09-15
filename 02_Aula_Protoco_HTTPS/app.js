3cf8777a2b53c0a82eb264ed8316e32d57057275
Eduardo Rocha <eduardomacedo1502@gmail.com>
2023-09-15 14:09:30 -0400

    rotas com node js

--- 02_Aula_Protoco_HTTPS/app.js
+++ 02_Aula_Protoco_HTTPS/app.js
@@ -8,3 +8,5 @@ HTTP.createServer(function(req,res){  // res e req fazem parte da função callb
 }).listen(8081); // criando um servidor e listando a porta a utilizar
 
 console.log("O servidor esta rodando !!")
+
+
---
+++ 03_Aula_Introducao_ao_Express/app.js
@@ -0,0 +1,27 @@
+const express = require('express')  // importanto o modulo do express
+
+const app = express() // criando a instanticia do express na const app
+
+app.get("/", function(req,res){  // criando uma rota
+
+    res.send("seja bem vindo Eduardo!!")
+
+})
+
+app.get("/home", function(req,res){ 
+
+    res.send("./index.html")
+
+})
+
+app.get("/pagina_vendas", function(req,res){ 
+
+    res.send("seja bem vindo a pagina de vendas !!")
+
+})
+
+
+
+app.listen(8081, ()=>{ // liganod servidor e utilizando uma função callbeck
+    console.log("servidor rodando na porta 8081")
+})
