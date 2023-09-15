8fad58d0c63f003585de22bf8e907d5a69aafb64
Eduardo Rocha <eduardomacedo1502@gmail.com>
2023-09-15 11:33:25 -0400

    Criando servidor http sem expres

---
+++ 02_Aula_Protoco_HTTPS/app.js
@@ -0,0 +1,10 @@
+var HTTP = require('http');   // importando o modulo http
+
+
+HTTP.createServer(function(req,res){  // res e req fazem parte da função callbeck 
+
+    res.end("ola seja bem vindo")
+
+}).listen(8081); // criando um servidor e listando a porta a utilizar
+
+console.log("O servidor esta rodando !!")
